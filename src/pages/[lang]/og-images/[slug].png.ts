01f2fca4778402fff75699b6df9ead06d5e75115
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-11-26 13:17:31 +0800

    feat: use ssr to generate og-images

--- src/pages/[lang]/og-images/[slug].png.ts
+++ src/pages/[lang]/og-images/[slug].png.ts
@@ -1,8 +1,10 @@
----
+import type { APIRoute } from "astro";
 import { getCollection, getEntry } from "astro:content";
 import { type Lang, ui } from "@/utils/i18n";
 import { classifyByLangs } from "@/utils/post";
 import { generateOgImageForPost } from "@/utils/og";
+
+export const prerender = false;
 
 export async function getStaticPaths() {
   const posts = await getCollection("posts");
@@ -21,18 +23,22 @@ export async function getStaticPaths() {
   return paths;
 }
 
-const { post } = Astro.props;
-const { lang, slug } = Astro.params;
+export const GET: APIRoute = async ({ props, params }) => {
+  const { post } = props;
+  const { lang, slug } = params;
 
-if (!post) {
-  for (const possibleLang of Object.keys(ui)) {
-    if (await getEntry("posts", `${possibleLang}/${slug}`)) {
-      return Astro.rewrite(`/${possibleLang}/og-images/${slug}.png`);
+  if (!post) {
+    for (const possibleLang of Object.keys(ui)) {
+      if (await getEntry("posts", `${possibleLang}/${slug}`)) {
+        return new Response(null, {
+          status: 302,
+          headers: { Location: `/${possibleLang}/og-images/${slug}.png` },
+        });
+      }
     }
+    return new Response("Not Found", { status: 404 });
   }
-  return Astro.rewrite("/404");
+  return new Response(await generateOgImageForPost(lang as Lang, post!), {
+    headers: { "Content-Type": "image/png" },
+  });
 }
-return new Response(await generateOgImageForPost(lang as Lang, post!), {
-  headers: { "Content-Type": "image/png" },
-});
----
