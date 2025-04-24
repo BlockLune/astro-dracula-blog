ac7f1909e6767ad3617f4257838fc9108eacda1e
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-04-24 14:55:02 +0800

    feat: api endpoint of full snapshot list

---
+++ src/pages/[lang]/posts/snapshots/index.json.ts
@@ -0,0 +1,20 @@
+import { getCollection } from "astro:content";
+import { getSnapshots } from "@/utils/post";
+import { type Lang, supportedLangs } from "@/utils/i18n";
+
+export const GET = async ({ params, request }) => {
+  const lang = params.lang as Lang;
+
+  const posts = await getCollection("posts");
+  const snapshots = await getSnapshots(posts, lang);
+
+  return new Response(
+    JSON.stringify(
+      snapshots.map((snapshot, index) => ({ id: index + 1, ...snapshot })),
+    ),
+  );
+};
+
+export async function getStaticPaths() {
+  return supportedLangs.map((lang) => ({ params: { lang } }));
+}
