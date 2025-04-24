7481598d9ea270fcc7493f8f734b74e3d930fb95
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-04-24 15:01:25 +0800

    feat: disable dynamic fetch on the simple post stack component

--- src/components/ui/post-stack.tsx
+++ src/components/ui/post-stack.tsx
@@ -2,7 +2,7 @@ import Fuse from "fuse.js";
 import PostCard from "@/components/ui/cards/post-card";
 import type { PostSnapshot } from "@/schemas/post";
 import { type Lang, useTranslations } from "@/utils/i18n";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import SearchInput from "@/components/ui/search-input";
 import { useSearchParams } from "@/hooks/use-search-params";
 
@@ -18,57 +18,23 @@ export default function PostStack({
   snapshots: PostSnapshot[];
 }) {
   const t = useTranslations(lang);
-  const numberOfPosts = snapshots.length;
   const { query, debouncedQuery, setQuery } = useSearchParams();
-
-  const [visiblePostsCount, setVisiblePostsCount] = useState(5);
-  const visiblePostsLengthRef = useRef(visiblePostsCount);
   const debouncedQueryRef = useRef(debouncedQuery);
 
   useEffect(() => {
     debouncedQueryRef.current = debouncedQuery;
   }, [debouncedQuery]);
 
-  const visiblePosts = useMemo(() => {
+  const searchResults = useMemo(() => {
     if (debouncedQuery === "") {
-      return snapshots.slice(0, visiblePostsCount);
+      return snapshots;
     }
     const fuse = new Fuse(snapshots, fuseOptions);
     return fuse
       .search(debouncedQuery)
       .map((result) => result.item)
       .slice(0, 5);
-  }, [debouncedQuery, snapshots, visiblePostsCount]);
-
-  useEffect(() => {
-    visiblePostsLengthRef.current = visiblePosts.length;
-  }, [visiblePosts.length]);
-
-  const sentinelRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const entry = entries[0];
-        if (
-          entry.isIntersecting &&
-          debouncedQueryRef.current === "" &&
-          visiblePostsLengthRef.current < numberOfPosts
-        ) {
-          setVisiblePostsCount((prev) => prev + 5);
-        }
-      },
-      { threshold: 1.0 },
-    );
-
-    const currentSentinel = sentinelRef.current;
-    currentSentinel && observer.observe(currentSentinel);
-
-    return () => {
-      currentSentinel && observer.unobserve(currentSentinel);
-      observer.disconnect();
-    };
-  }, []);
+  }, [debouncedQuery, snapshots]);
 
   return (
     <div className="flex flex-col gap-4">
@@ -76,13 +42,13 @@ export default function PostStack({
         lang={lang}
         query={query}
         onChange={setQuery}
-        totalCount={numberOfPosts}
+        totalCount={snapshots.length}
         type="post"
       />
 
-      {visiblePosts.length > 0 ? (
+      {searchResults.length > 0 ? (
         <>
-          {visiblePosts.map((snapshot) => (
+          {searchResults.map((snapshot) => (
             <PostCard
               lang={lang}
               snapshot={snapshot}
@@ -90,18 +56,6 @@ export default function PostStack({
               key={snapshot.slug}
             />
           ))}
-
-          {debouncedQuery === "" && visiblePosts.length < numberOfPosts && (
-            <section
-              ref={sentinelRef}
-              aria-labelledby="observer-label"
-              className="h-2 w-full"
-            >
-              <h2 id="observer-label" className="sr-only">
-                Load more trigger
-              </h2>
-            </section>
-          )}
         </>
       ) : (
         <p className="text-center">{t("search.noResults")}</p>
