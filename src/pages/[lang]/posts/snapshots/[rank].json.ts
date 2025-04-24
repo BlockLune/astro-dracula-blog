d7f99ee1470910386f8d6bb5803ee94de9ca0153
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-04-24 23:46:19 +0800

    fix: static paths of [rank].json endpoint

--- src/pages/[lang]/posts/snapshots/[rank].json.ts
+++ src/pages/[lang]/posts/snapshots/[rank].json.ts
@@ -6,28 +6,36 @@ import { type Lang, supportedLangs } from "@/utils/i18n";
 
 export const GET: APIRoute = async ({ params }) => {
   const lang = params.lang as Lang;
-  const rank = params.rank;
+  const rank = Number(params.rank);
 
   const posts = await getCollection("posts");
   const snapshots = await getSnapshots(posts, lang);
 
   return new Response(
     JSON.stringify(
-      snapshots
-        .map((snapshot, index) => ({ rank: index + 1, ...snapshot }))
-        .find((snapshot) => snapshot.rank === Number(rank)),
+      snapshots.map((snapshot, index) => ({ rank: index + 1, ...snapshot }))[
+        rank - 1
+      ],
     ),
   );
 };
 
 export async function getStaticPaths() {
   const posts = await getCollection("posts");
-  return supportedLangs.flatMap((lang) =>
-    posts.map((_, index) => ({
-      params: {
-        lang,
-        rank: String(index + 1),
-      },
-    })),
-  );
+
+  const paths: { params: { lang: Lang; rank: string } }[] = [];
+
+  for (const lang of supportedLangs) {
+    const snapshots = await getSnapshots(posts, lang);
+    snapshots.forEach((_, index) => {
+      paths.push({
+        params: {
+          lang,
+          rank: String(index + 1),
+        },
+      });
+    });
+  }
+
+  return paths;
 }
